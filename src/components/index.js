360d34d05183226d340e261cdb502e30b0e8419e
najmulhouda <najmulhouda6@gmail.com>
2024-01-25 21:10:07 +0600

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+import AuthLayout from "./AuthLayout";
+import Button from "./Button";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
+import Login from "./Login";
+import Logo from "./Logo";
+import PostCard from "./PostCard";
+import RTE from "./RTE";
+import Select from "./Select";
+import Signup from "./Signup";
+import Container from "./container/Container";
+import PostForm from "./post-form/PostForm";
+export {
+    AuthLayout, Button, Container, Footer, Header, Input, Login, Logo,
+    LogoutBtn, PostCard, PostForm, RTE, Select, Signup
+};
